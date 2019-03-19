fb60018d93888113dc9bcf662aeca320fcc3fb1b
weichun <kickdesk@gmail.com>
2019-03-18 23:41:40 -0500

    Update vizwiz.py

--- vizwiz.py
+++ vizwiz.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# Lab assignment 4 foundation
+#!/usr/bin/env/python3.6
+# Lab assignment 4 
 # - Working with the VizWiz dataset
 import os
 import json
@@ -8,4 +8,35 @@ import requests
 
 base_url = 'https://ivc.ischool.utexas.edu/VizWiz/data'
 img_dir = '%s/Images/' %base_url
-print(img_dir)
+
+# Retrieve file from ULR and store it locally
+split = 'train'
+train_file = '%s/Annotations/%s.json' %(base_url,split)
+train_data = requests.get(train_file, allow_redirects=True)
+print(train_file)
+
+# Read the local file
+num_train_VQs = 4
+training_data = train_data.json()
+for vq in training_data[0:num_train_VQs]:
+    image_name = vq['image']
+    question = vq['question']
+    label = vq['answerable']
+    print(image_name)
+    print(question)
+    print(label)
+    
+split = 'val'
+val_file = '%s/Annotations/%s.json' %(base_url, split)
+val_data = requests.get(val_file, allow_redirects=True)
+print(val_file)
+
+numValVQs = 3
+validation_data = val_data.json()
+for vq in validation_data[0:numValVQs]:
+    image_name = vq['image']
+    question = vq['question']
+    label = vq['answerable']
+    print(image_name)
+    print(question)
+    print(label)
