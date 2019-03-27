021f5b6c0acabaa47a0d09f4bbe3ee563caaebae
weichun <kickdesk@gmail.com>
2019-03-26 19:46:42 -0500

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -22,10 +22,10 @@ X_train_std = stdsc.transform(X_train)
 X_test_std = stdsc.transform(X_test)
 
 # Optimize hyperparameters
-mlp = MLPClassifier(max_iter=20, random_state=42, verbose=True, hidden_layer_sizes=(10,))
+mlp = MLPClassifier(max_iter=20, random_state=42, verbose=True, hidden_layer_sizes=(60,60))
 mlp.fit(X_train_std, y_train)
 print("Accuracy on the test set: {:.2f}".format(mlp.score(X_test_std, y_test)))
 print("Activation function used at the output layer: %s" % mlp.out_activation_)
 print("Number of outputs at the output layer: %f" % mlp.n_outputs_)
 print("List predicted classes at the output layer: %s" % mlp.classes_)
-print("Training set loss: %s" % mlp.loss_)1
+print("Training set loss: %s" % mlp.loss_)
